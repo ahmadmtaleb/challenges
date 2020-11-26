0cbf4c7442839fd074a0dedcffc6bef7410666d7
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-11-26 19:10:58 +0200

    power of two solved

--- powersOfTwo.js
+++ powersOfTwo.js
@@ -1,22 +1,22 @@
 /*Powers of Two
-HIDE QUESTION
 Have the function PowersofTwo(num) take the num parameter being passed which will be an integer and return the string true if it's a power of two. If it's not return the string false. For example if the input is 16 then your program should return the string true but if the input is 22 then the output should be the string false.
 
 Use the Parameter Testing feature in the box below to test your code with different arguments.
 */
-
-/* Solution 
-def MathChallenge(num):
-
-  # code goes here
-  while num%2==0:
-    num=num/2
-  if num==1:
-    return 'true'
-  else:
-    return 'false'
-  #return num
-
-# keep this function call here 
-print(MathChallenge(input()))
-*/
+function PowersofTwo(num){
+  let x = Math.sqrt(num);
+  if(num === 0){
+    return 'false';
+  }
+  if(Math.ceil((Math.log(num) / Math.log(2))) == Math.floor(((Math.log(num) / Math.log(2))))){
+    return 'true';
+  }
+  return 'false';
+}
+console.log(PowersofTwo(22)); // Output: false
+console.log(PowersofTwo(16)); // Output: true
+console.log(PowersofTwo(25)); // Output: false
+console.log(PowersofTwo(12)); // Output: false
+console.log(PowersofTwo(32)); // Output: true
+console.log(PowersofTwo(8)); // Output: true
+console.log(PowersofTwo(64)); // Output: true
