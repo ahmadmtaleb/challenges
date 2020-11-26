88c01b2ac7815c82996ff870181f55ce4370b845
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2020-11-26 19:27:33 +0200

    prime time solved

--- primeTime.js
+++ primeTime.js
@@ -1,25 +1,18 @@
 /*
  Prime Time
-HIDE QUESTION
 Have the function PrimeTime(num) take the num parameter being passed and return the string true if the parameter is a prime number, otherwise return the string false. The range will be between 1 and 2^16.
 
 Use the Parameter Testing feature in the box below to test your code with different arguments.
 */
-/**
- * Solution in python
- * def MathChallenge(num):
 
-  # code goes here
-  flag=0
-  for i in range(2,num):
-    if num%i == 0:
-      flag=1
-      break
-  if flag == 0:
-    return 'true'
-  else:
-    return 'false'
-
-# keep this function call here 
-print(MathChallenge(input()))
- */
+function PrimeTime(num){
+  for(let i = 2; i <= Math.sqrt(num); i++){
+    if(num % i == 0){
+      return 'false';
+    }
+  }
+  return 'true';
+}
+for(let i = 0; i <= 23; i++){
+  console.log(i, PrimeTime(i));
+}
