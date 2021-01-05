b60d4ccfc11334b0f5b576b86ed87bba87ecbe8a
ahmadmtaleb <ahmedmtaleb.23@gmail.com>
2021-01-05 19:57:27 +0200

    longest substring without repeating characters

---
+++ 17-longestSubstringWithoutRepeatingCharacters.js
@@ -0,0 +1,21 @@
+// This problem was recently asked by Microsoft:
+
+// Given a string, find the length of the longest substring without repeating characters.
+
+/**
+ * |      Input                     |   Output  
+ * |    s = "abcabcbb"              |     3
+ * |    s = "bbbbb"                 |     1
+ * |    s = "pwwkew"                |     3
+ * |    s = ""                      |     0
+ * |    s = "ABDEFGABEF"            |     6
+ * |    s = "GEEKSFORGEEKS"         |     7
+ * |    s = "abrkaabcdefghijjxxx"   |    10
+ */
+
+
+function longestSubstring(str){
+
+    return length;
+}
+console.log(longestSubstring());
